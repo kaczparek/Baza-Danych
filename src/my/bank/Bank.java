447ddc5fdcb5d29e4d539e1ae79d3e737eb45c17
unknown <Szymon Kaczmarek>
2019-01-15 20:59:13 +0100

    Gui.v2

--- src/my/bank/Bank.java
+++ src/my/bank/Bank.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 
